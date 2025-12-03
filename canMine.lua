afb71e35ed992be60bd82eca2f15a4c42c3b06f7
The-Forger <asojdjaosjdhaosdj@gmail.com>
2025-12-03 19:28:16 +0100

    Update canMine.lua

--- canMine.lua
+++ canMine.lua
@@ -1,4 +1,8 @@
 return {
   ["Stone Pickaxe"]={"Pebble"};
   ["Bronze Pickaxe"]={"Pebble","Rock"};
+  ["Iron Pickaxe"]={"Pebble","Rock"};
+  ["Gold Pickaxe"]={"Pebble","Rock","Boulder"};
+  ["Platinum Pickaxe"]={"Pebble","Rock","Boulder","Basalt Rock"};
+  ["Cobalt Pickaxe"]={"Pebble","Rock","Boulder","Basalt Rock","Basalt Core"};
 }
