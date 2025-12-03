3734e49d4c76872ef7b6a50929e19e574c2ed428
The-Forger <asojdjaosjdhaosdj@gmail.com>
2025-12-03 18:33:11 +0100

    Update forgehelp.lua

--- forgehelp.lua
+++ forgehelp.lua
@@ -1,19 +1,16 @@
 local forgehelp = {}
 local warn = function(...) warn("[FORGEHELP] >",...)
-
-forgehelp.encode = function(a)
-  return game:GetService("HttpService"):JSONEncode(a)
-end
   
 forgehelp.swing = function(...)
   local args = ...
-  warn("module.swing > ",forgehelp.encode(args),"typeof value parsed:",typeof(args))
+  local json = game:GetService("HttpService"):JSONEncode(args)
+  warn("module.swing > ",json,"typeof value parsed:",typeof(args))
   
   if not typeof(args)=="table" then return end if #args>1 or #args==0 then return end
   warn(1)
   if args[1]==1 then args[1]="Pickaxe" end
   if args[1]==2 then args[1]="Weapon" end
-  warn(2,forgehelp.encode(args))
+  warn(2,json)
   if not args[1] or (args[1]~="Weapon" or args[1]~="Pickaxe") then return end
   warn(3)
   game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("ToolActivated"):InvokeServer(unpack(args))
