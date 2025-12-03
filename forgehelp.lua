401183c13f94aaf7e52ae9cf496c6fc342d7e388
The-Forger <asojdjaosjdhaosdj@gmail.com>
2025-12-03 17:46:59 +0100

    Create forgehelp.lua

---
+++ forgehelp.lua
@@ -0,0 +1,12 @@
+local forgehelp = {}
+
+forgehelp.swing = function(...)
+  local args = ...
+  if not typeof(args)=="table" then return end if args[2] then return end
+  if args[1]==1 then args[1]="Pickaxe" end
+  if args[1]==2 then args[1]="Weapon" end
+  if not args[1] or (args[1]~="Weapon" or args[1]~="Pickaxe") then return end
+  game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("ToolActivated"):InvokeServer(unpack(args))
+end
+
+return forgehelp
